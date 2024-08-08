6668fa939c8ba56e69eecf827bdead0a4059fb92
ManaasGupta <manasgpt3@gmail.com>
2024-08-08 18:25:35 +0530

    Initial Commit

---
+++ src/test/java/com/base/jenkinsdockerproject/JenkinsdockerprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.base.jenkinsdockerproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsdockerprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
